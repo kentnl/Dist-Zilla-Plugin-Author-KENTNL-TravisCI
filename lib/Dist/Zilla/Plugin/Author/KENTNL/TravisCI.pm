44e1ccc93bf97563b119f9cc55b5dc5327cd9cf6
Kent Fredric <kentfredric@gmail.com>
2015-01-02 10:33:09 +1300

    Build results of 5aa18c5 (on master)

---
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/TravisCI.pm
@@ -0,0 +1,74 @@
+use 5.006;    # our
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::Author::KENTNL::TravisCI;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT: A specific subclass of TravisCI that does horrible things
+
+our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
+
+use Moose qw( extends );
+extends 'Dist::Zilla::Plugin::TravisCI';
+
+sub modify_travis_yml {
+  my ( $self, %yaml ) = @_;
+  my $allow_failures = [
+    { perl => "5.8" },
+    { env  => "STERILIZE_ENV=0 RELEASE_TESTING=1 AUTHOR_TESTING=1" },
+    { env  => "STERILIZE_ENV=0 DEVELOPER_DEPS=1" },
+  ];
+  my $include = [
+    { perl => "5.21", env => "STERILIZE_ENV=0 COVERAGE_TESTING=1" },
+    { perl => "5.21", env => "STERILIZE_ENV=1" },
+    ( map { +{ perl => $_, env => "STERILIZE_ENV=0" } } "5.8", "5.10", "5.12", "5.14", "5.16", "5.20", "5.21", ),
+    ( map { +{ perl => $_, env => "STERILIZE_ENV=1" } } "5.8", "5.10", "5.20", ),
+    { perl => "5.21", env => "STERILIZE_ENV=0 DEVELOPER_DEPS=1" },
+    { perl => "5.21", env => "STERILIZE_ENV=0 RELEASE_TESTING=1 AUTHOR_TESTING=1" },
+  ];
+  $yaml{matrix} = {
+    allow_failures => $allow_failures,
+    include        => $include,
+  };
+
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::Plugin::Author::KENTNL::TravisCI - A specific subclass of TravisCI that does horrible things
+
+=head1 VERSION
+
+version 0.001000
+
+=head1 DESCRIPTION
+
+B<NO USER SERVICABLE PARTS INSIDE>
+
+B<CHOKING HAZARD>
+
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
