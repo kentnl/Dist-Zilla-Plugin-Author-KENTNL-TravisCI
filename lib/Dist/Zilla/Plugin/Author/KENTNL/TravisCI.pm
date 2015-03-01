9bccf3cdaedf9492412b8f66bb7c147492182ad3
Kent Fredric <kentfredric@gmail.com>
2015-03-01 22:11:15 +1300

    Switch from build/master to builds

--- lib/Dist/Zilla/Plugin/Author/KENTNL/TravisCI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/TravisCI.pm
@@ -63,7 +63,7 @@ sub modify_travis_yml {
   $yaml{before_script} = [ 'time perl ./maint-travis-ci/before_script.pl', ];
   $yaml{script}        = [ 'time perl ./maint-travis-ci/script.pl', ];
   $yaml{after_failure} = [ 'perl ./maint-travis-ci/report_fail_ctx.pl', ];
-  $yaml{branches}      = { only => [ 'master', 'build/master', 'releases', ] };
+  $yaml{branches}      = { only => [ 'master', 'builds', 'releases', ] };
   $yaml{sudo}          = 'false';
   delete $yaml{perl};
 
