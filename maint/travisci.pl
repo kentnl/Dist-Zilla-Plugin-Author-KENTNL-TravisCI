d0461de7432270b819c804990ce6952549fa90f5
Kent Fredric <kentfredric@gmail.com>
2017-02-28 18:53:28 +1300

    Upgrade compile-per-file

--- maint/travisci.pl
+++ maint/travisci.pl
@@ -2,5 +2,5 @@
 use strict;
 use warnings;
 return sub {
-  unshift @{ $_[0]->{install} }, 'time cpanm --quiet --notest --no-man-pages --dev Dist::Zilla::Plugin::Test::Compile::PerFile';
+  push @{ $_[0]->{install} }, 'time cpanm --quiet --notest --no-man-pages --dev Dist::Zilla::Plugin::Test::Compile::PerFile';
 };
